fbf6faff00011d89d08690a49a7852ca2143d4c2
Douglas Henrique <barbozadh99@gmail.com>
2020-09-16 19:39:56 -0300

    Add examples with Chats usage Vue. Library: ApexCharts

--- collect-tool/src/App.vue
+++ collect-tool/src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <timeline :data="[['test1', '2001-00-01', '2002-00-01'],
+                      ['test2', '2002-00-01', '2003-00-01'],
+                      ['test3', '2003-00-01', '2004-00-01']]">
+    </timeline>
+  </div>
 </template>
 
 <script>
--- collect-tool/src/main.js
+++ collect-tool/src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import 'spectre.css/dist/spectre.min.css';
-import 'spectre.css/dist/spectre-icons.min.css';
-
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
 Vue.component('l-map', LMap);
