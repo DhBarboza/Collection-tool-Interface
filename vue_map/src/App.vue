d74cd91bb0493b3445b6d5107ef693e109c30c28
Douglas Henrique <barbozadh99@gmail.com>
2020-09-17 20:27:28 -0300

    Add test with leaflet. Incomplete: True

---
+++ vue_map/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div style="height: 500px; width: 100%">
+      <l-map
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        style="height: 80%"
+        @update:center="centerUpdate"
+        @update:zoom="zoomUpdate"
+      >
+        <l-tile-layer
+          :url="url"
+          :attribution="attribution"
+        />
+        <l-marker :lat-lng="withTooltip"></l-marker>
+      </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+export default {
+  name: "Example",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: latLng(-23.1791, -45.8872),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(-23.1791, -45.8872),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(-23.1791, -45.8872),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+  }
+};
+</script>
+
+<style>
+
+</style>
