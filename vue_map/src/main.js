62b31578479ca98eaeb468ebe49b625c6f7b7d14
Douglas Henrique <barbozadh99@gmail.com>
2020-09-18 13:00:03 -0300

    Add Test Vue Chart. Incomplet: True

--- vue_map/src/main.js
+++ vue_map/src/main.js
@@ -8,7 +8,7 @@ Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
 
-new Vue({
+new Vue ({
     el: '#app',
     template: '<App/>',
     components: { App },
